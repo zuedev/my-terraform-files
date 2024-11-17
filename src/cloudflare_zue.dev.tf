dc6600c1785a24579a61a70f26eaff617d1e8d23
zuedev <zuedev@gmail.com>
2024-11-17 14:53:31 +0000

    actual dns

--- src/cloudflare_zue.dev.tf
+++ src/cloudflare_zue.dev.tf
@@ -4,67 +4,15 @@ data "cloudflare_zones" "zue-dot-dev" {
   }
 }
 
-resource "cloudflare_record" "zue-dot-dev_test" {
+resource "cloudflare_record" "zue-dot-dev_caprover_a" {
   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
 
   comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
 
-  type    = "CNAME"
-  name    = "test.zue.dev"
-  value   = "zue.dev"
-  proxied = true
-
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "zue-dot-dev_test2" {
-  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
-
-  comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
-
-  type    = "CNAME"
-  name    = "test2.zue.dev"
-  value   = "zue.dev"
-  proxied = true
-
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "zue-dot-dev_test3" {
-  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
-
-  comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
-
-  type    = "CNAME"
-  name    = "test3.zue.dev"
-  value   = "zue.dev"
-  proxied = true
-
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "zue-dot-dev_test4" {
-  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
-
-  comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
-
-  type    = "CNAME"
-  name    = "test4.zue.dev"
-  value   = "zue.dev"
-  proxied = true
-
-  allow_overwrite = true
-}
-
-resource "cloudflare_record" "zue-dot-dev_test5" {
-  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
-
-  comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
-
-  type    = "CNAME"
-  name    = "test5.zue.dev"
-  value   = "zue.dev"
-  proxied = true
+  type    = "A"
+  name    = "caprover.zue.dev"
+  value   = "195.201.101.2"
+  proxied = false
 
   allow_overwrite = true
 }
