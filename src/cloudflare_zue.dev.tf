eec6770f27effd0a3448366baf64b6a7f9212921
zuedev <zuedev@gmail.com>
2024-11-17 15:05:27 +0000

    add wings583

--- src/cloudflare_zue.dev.tf
+++ src/cloudflare_zue.dev.tf
@@ -16,3 +16,16 @@ resource "cloudflare_record" "zue-dot-dev_caprover_a" {
 
   allow_overwrite = true
 }
+
+resource "cloudflare_record" "zue-dot-dev_wings583_a" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
+
+  type    = "A"
+  name    = "wings583.zue.dev"
+  value   = "94.130.142.48"
+  proxied = false
+
+  allow_overwrite = true
+}
