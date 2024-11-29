a692bdbd42ba2507c258abbcea3aa8124dd42997
Alex Pooley <zuedev@gmail.com>
2024-11-29 11:54:24 +0000

    remove repos#

--- src/gitlab_mirrors.tf
+++
@@ -1,7 +0,0 @@
-resource "gitlab_project" "gitlab_mirrors_zuedev" {
-  for_each         = toset(["my-terraform-files", "my-opentofu-files"])
-  name             = each.key
-  import_url       = "https://github.com/zuedev/${each.key}.git"
-  mirror           = true
-  visibility_level = "public"
-}
