4a058754e658182dcf6a8cc7cdc1ae93022367f4
Alex Pooley <zuedev@gmail.com>
2024-11-29 07:25:47 +0000

    replace current a records with aws ns ones

--- src/cloudflare_zue.dev.tf
+++ src/cloudflare_zue.dev.tf
@@ -4,28 +4,50 @@ data "cloudflare_zones" "zue-dot-dev" {
   }
 }
 
-resource "cloudflare_record" "zue-dot-dev_caprover_a" {
+resource "cloudflare_record" "zue-dot-dev_aws_ns_1" {
   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
 
   comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
 
-  type    = "A"
-  name    = "caprover.zue.dev"
-  value   = "195.201.101.2"
-  proxied = false
+  type    = "NS"
+  name    = "aws.zue.dev"
+  value   = "ns-1732.awsdns-24.co.uk"
 
   allow_overwrite = true
 }
 
-resource "cloudflare_record" "zue-dot-dev_wings583_a" {
+resource "cloudflare_record" "zue-dot-dev_aws_ns_2" {
   zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
 
   comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
 
-  type    = "A"
-  name    = "wings583.zue.dev"
-  value   = "94.130.142.48"
-  proxied = false
+  type    = "NS"
+  name    = "aws.zue.dev"
+  value   = "ns-1179.awsdns-19.org"
+
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "zue-dot-dev_aws_ns_3" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
+
+  type    = "NS"
+  name    = "aws.zue.dev"
+  value   = "ns-810.awsdns-37.net"
+
+  allow_overwrite = true
+}
+
+resource "cloudflare_record" "zue-dot-dev_aws_ns_4" {
+  zone_id = data.cloudflare_zones.zue-dot-dev.zones.0.id
+
+  comment = "TF-${formatdate("YYMMDDhhmmss", timestamp())}"
+
+  type    = "NS"
+  name    = "aws.zue.dev"
+  value   = "ns-298.awsdns-37.com"
 
   allow_overwrite = true
 }
