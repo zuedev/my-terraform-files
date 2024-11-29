5977c50c95eb89a70c45c7f9aca7d8d69ba2520b
Alex Pooley <zuedev@gmail.com>
2024-11-29 12:07:37 +0000

    add measure latency health check

--- src/aws_route53_health_checks.tf
+++ src/aws_route53_health_checks.tf
@@ -2,6 +2,7 @@ resource "aws_route53_health_check" "zue-dot-dev" {
   fqdn             = "zue.dev"
   type             = "HTTPS"
   request_interval = "30"
+  measure_latency  = true
 
   tags = {
     Name = "tf-zue-dot-dev"
