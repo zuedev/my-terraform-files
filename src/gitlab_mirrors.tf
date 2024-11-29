312e295da6afef9dec12af19b97594e6656451b5
Alex Pooley <zuedev@gmail.com>
2024-11-29 11:47:29 +0000

    change visibility of gitlab_mirrors.tf

--- src/gitlab_mirrors.tf
+++ src/gitlab_mirrors.tf
@@ -1,5 +1,6 @@
 resource "gitlab_project" "gitlab_mirror_my-terraform-files" {
-  name       = "my-terraform-files"
-  import_url = "https://github.com/zuedev/my-terraform-files.git"
-  mirror     = true
+  name             = "my-terraform-files"
+  import_url       = "https://github.com/zuedev/my-terraform-files.git"
+  mirror           = true
+  visibility_level = "public"
 }
